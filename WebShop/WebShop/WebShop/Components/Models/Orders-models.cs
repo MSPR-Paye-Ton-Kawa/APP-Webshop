f87d99935703a58c1cce5591b14f96282c1af1b4
Colas Rabiller <colas.rabiller@gmail.com>
2024-10-26 14:39:19 +0200

    order page + change url + add models structure

---
+++ WebShop/WebShop/WebShop/Components/Models/Orders-models.cs
@@ -0,0 +1,40 @@
+﻿namespace WebShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }  // Managed externally
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public ICollection<Payment> Payments { get; set; }
+
+        // Nouvelle propriété pour stocker le montant total
+        public decimal TotalAmount { get; set; }
+        public string CustomerName { get; set; }
+
+    }
+
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+
+        public Order? Order { get; set; }
+        public int ProductId { get; set; }  // Managed externally
+        public int Quantity { get; set; }
+    }
+
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int OrderId { get; set; }
+        public Order? Order { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ WebShop/WebShop/WebShop/Components/Models/Products-models.cs
@@ -0,0 +1,31 @@
+﻿namespace WebShop.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = new Category();
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; } = new Supplier();
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryType { get; set; }
+    }
+
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactEmail { get; set; }
+    }
+}
